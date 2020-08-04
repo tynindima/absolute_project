5010c35ad1c25688e5bdfeeaefcf86837f09541d
tynindima <tynindima@gmail.com>
2020-08-04 15:08:47 +0300

    added header

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,20 @@ module.exports = {
     es6: true,
     browser: true,
     node: true,
+    jquery: true
   },
-  extends: ["airbnb-base", "query"],
+  extends: [
+    "airbnb-base",
+    "jquery",
+    "plugin:jquery/slim"
+  ],
+  plugins: [
+    "jquery"
+  ],
+  rules: {
+    "prefer-arrow-callback": "off",
+    quotes: "off",
+    "func-names": "off",
+    "space-in-parens": "off"
+  }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  function checkWindowSize() {
+    const width = $(window).width();
+    const menuIsShow = width >= 1200 ? 'show' : '';
+    const searchIsShow = width >= 768 ? 'show' : '';
+    $('#menu-cnt-top').removeClass('show').addClass(menuIsShow);
+    $('#input-cnt-top').removeClass('show').addClass(searchIsShow);
+  }
+  checkWindowSize();
 
-  $('#menu-button').on('click', function () {
+  $('#menu-button').on('click', function() {
     $(this).toggleClass('open');
+    $('#menu-cnt-top').collapse('toggle');
   });
 
-  $('#search-mobile-btn').on('click', function () {
-      $(this).attr('src', function(_, attr) {
-        return attr === './img/header/search-mobile.png' ? './img/header/search-close.svg' : './img/header/search-mobile.png';
-      });
+  $('#search-mobile-btn').on('click', function() {
+    $(this).attr('src', function(_, attr) {
+      return attr === './img/header/search-mobile.png' ?
+        './img/header/search-close.svg' :
+        './img/header/search-mobile.png';
+    });
 
+    $('#input-cnt-top').collapse('toggle');
   });
 });
