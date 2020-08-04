b491e8130523fdf1d06e2a80fe3f097499e4d92a
tynindima <tynindima@gmail.com>
2020-08-04 23:59:47 +0300

    Added inputs section

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-  function checkWindowSize() {
+  function resize() {
     const width = $(window).width();
     const menuIsShow = width >= 1200 ? 'show' : '';
     const searchIsShow = width >= 768 ? 'show' : '';
+
     $('#menu-cnt-top').removeClass('show').addClass(menuIsShow);
     $('#input-cnt-top').removeClass('show').addClass(searchIsShow);
   }
-  checkWindowSize();
+
+  $(window).on('resize', resize).trigger('resize');
 
   $('#menu-button').on('click', function() {
     $(this).toggleClass('open');
